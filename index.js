87149e055a2255a3636b9b2010547d2f4d559e1a
Thomas Ridings <thomas.a.ridings@gmail.com>
2022-07-17 22:17:40 +0100

    Fixing promises

--- index.js
+++ index.js
@@ -102,9 +102,13 @@ exports.handler = async (event) => {
 };
 
 function getCombined(){
-    var subscription = getSubcription();
-    var oneOff = getOneOff();
+    var subscription = await getSubcription();
+    var oneOff = await getOneOff();
+
     return Promise.all(subscription, oneOff)
+    .then(data => {
+        return data[0].concat(data[1])
+    })
 }
 
 function getOneOff(){
